3c24fe3d2dff99630b225ab1ad7ac9189e2156b6
somaa1 <ismailrabie111@gmail.com>
2023-07-31 11:44:22 +0300

    commit => doing soma code like(delete from firebase with alert)

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,39 +1,154 @@
+import 'package:chatapp/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import '../constant.dart';
+import '../models/message.dart';
 import '../widgets/chate_bubble.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ChatPage extends StatefulWidget {
-  const ChatPage({super.key});
-
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final ScrollController _controller = ScrollController();
+
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+
+  CollectionReference messages =
+      FirebaseFirestore.instance.collection(KMessageCollection);
+  TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-        backgroundColor: KPrimryColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              KLogo,
-              width: 50,
+    String? email = ModalRoute.of(context)!.settings.arguments as String?;
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: messages.orderBy(KCreatedAt, descending: true).snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<Messages> messageslist = [];
+          for (int i = 0; i < snapshot.data!.docs.length; i++) {
+            messageslist.add(Messages.fromJson(snapshot.data!.docs[i]));
+          }
+          return Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              centerTitle: true,
+              backgroundColor: KPrimryColor,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    KLogo,
+                    width: 50,
+                  ),
+                  Text("Chat")
+                ],
+              ),
             ),
-            Text("Chat")
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    reverse: true,
+                    controller: _controller,
+                    itemCount: messageslist.length,
+                    itemBuilder: (context, index) {
+                      return messageslist[index].id == email
+                          ? chatbubble(
+                              message: messageslist[index],
+                              send: () async {
+                                 deleteMessage(snapshot.data!.docs[index].id);
+                              },
+                            )
+                          : chatbubbleforFriend(
+                              message: messageslist[index],
+                            );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextField(
+                    controller: controller,
+                    onSubmitted: (data) {
+                      messages.add({
+                        KMessage: data,
+                        KCreatedAt: DateTime.now(),
+                        KId: email
+                      });
+                      controller.clear();
+                      _controller.animateTo(
+                        //    _controller.position.maxScrollExtent,
+                        0,
+                        duration: Duration(milliseconds: 500),
+                        curve: Curves.fastOutSlowIn,
+                      );
+                    },
+                    decoration: InputDecoration(
+                        hintText: 'Send Message',
+                        suffixIcon: Icon(
+                          Icons.send,
+                          color: KPrimryColor,
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16)),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide(color: KPrimryColor))),
+                  ),
+                )
+              ],
+            ),
+          );
+        } else {
+          return Text('Loading....');
+        }
+      },
+    );
+  }
+
+
+  void deleteMessage(String messageId) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Delete Message"),
+          content: Text("Are you sure you want to delete this message?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the AlertDialog
+              },
+              child: Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                // Perform the deletion
+
+                messages.doc(messageId).delete();
+                Navigator.pop(context);
+                Fluttertoast.showToast(
+                    msg: "Deleted successful",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.CENTER,
+                    timeInSecForIosWeb: 1,
+                    backgroundColor: Colors.red,
+                    textColor: Colors.white,
+                    fontSize: 16.0
+                );
+
+                // Close the AlertDialog
+              },
+              child: Text("Delete"),
+            ),
           ],
-        ),
-      ),
-      body: ListView.builder(itemBuilder: (context, index) {
-       return chatbubble();
-      },)
+        );
+      },
     );
   }
 }
-
-
