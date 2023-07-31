25cf09b84b21433e296981770d197b8af239d849
somaa1 <ismailrabie111@gmail.com>
2023-07-31 11:44:50 +0300

    commit => New widget name chatbubbleforFriend

--- lib/widgets/chate_bubble.dart
+++ lib/widgets/chate_bubble.dart
@@ -1,31 +1,64 @@
+import 'package:chatapp/models/message.dart';
 import 'package:flutter/material.dart';
 
 import '../constant.dart';
+
 class chatbubble extends StatelessWidget {
-  const chatbubble({
-    super.key,
-  });
+  Messages message;
+  VoidCallback? send;
+
+  chatbubble({super.key, required this.message,this.send});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Align(
       alignment: Alignment.centerLeft,
-      padding: EdgeInsets.only(left: 20),
-      height: 60,
-      margin: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(32),
-            topRight: Radius.circular(32),
-            bottomRight: Radius.circular(32)),
-        color: KPrimryColor,
+      child: Container(
+        padding: EdgeInsets.only(left: 20, top: 16, bottom: 16, right: 16),
+        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(32),
+              topRight: Radius.circular(32),
+              bottomRight: Radius.circular(32)),
+          color: KPrimryColor,
+        ),
+        child: GestureDetector(
+       onLongPress: send,
+          child: Text(
+            message.message,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
       ),
-      child:
-      Text(
-        "iam somaa",
-        style: TextStyle(color: Colors.white60),
-      ),
+    );
+  }
+}
 
+class chatbubbleforFriend extends StatelessWidget {
+  Messages message;
+
+  chatbubbleforFriend({super.key, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Container(
+        padding: EdgeInsets.only(left: 20, top: 16, bottom: 16, right: 16),
+        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(32),
+              topRight: Radius.circular(32),
+              bottomLeft: Radius.circular(32)),
+          color: KSecondryColor,
+        ),
+        child: Text(
+          message.message,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
     );
   }
 }
