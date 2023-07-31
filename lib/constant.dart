7740ee211983eab87ae772b7d3552ae5e051e8df
somaa1 <ismailrabie111@gmail.com>
2023-07-31 11:45:13 +0300

    commit => add some constant

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,4 +1,9 @@
 import 'dart:ui';
-
+import 'package:flutter/material.dart';
 const KPrimryColor=Color(0Xff274460);
+const KSecondryColor=Color(0Xff006488);
 const KLogo="assets/images/scholar.png";
+const KMessageCollection='messages';
+const KMessage='message';
+const KCreatedAt='createdAt';
+const KId='id';
